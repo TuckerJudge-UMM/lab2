5c83f14a010a4098e223fb144424f81358ad2a9d
JGuadalupeD <150277290+JGuadalupeD@users.noreply.github.com>
2026-09-14 22:45:12 -0500

    Write the quicksort 'switch two insertion sort' modification

---
+++ QuicksortInsertion.java
@@ -0,0 +1,60 @@
+import java.util.Random;
+
+public class QuicksortInsertion {
+    public void qsort(TestInteger[] arr, int p, int r){
+        if(p + 5 < r){
+            int q = Partition(arr, p, r);
+
+            qsort(arr, p, q-1);
+            qsort(arr, q+1, r);
+        } else if (p < r){
+            insertionSort(arr, p, r); // Need to implement this fnc
+        }
+    }
+
+    public void insertionSort(TestInteger[] arr, int p, int r){
+        for(int i = p; p < r; i++){
+            TestInteger key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j].compareTo(key) > 0){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+
+    public int Partition(TestInteger[] arr, int p, int r){
+        TestInteger x = arr[r];
+        int i = p-1;
+        for(int j=p; j<r; j++){
+            // stable??
+            if(arr[j].compareTo(x)<=0){
+                i=i+1;
+                TestInteger temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        TestInteger temp = arr[i+1];
+        arr[i+1] = arr[r];
+        arr[r] = temp;
+
+        return i+1;
+    }
+    public static void main(String[] args){
+        Random gen = new Random();
+        int n = 10000;
+        TestInteger[] arr = new TestInteger[n];
+        for (int i = 0; i<n;i++){
+            TestInteger num = new TestInteger(gen.nextInt(0,1000000));
+            arr[i] = num;
+        }
+
+        Quicksort qs = new Quicksort();
+        qs.qsort(arr,0, arr.length-1);
+        for (int i = 0; i<n;i++){
+            System.out.println(arr[i].value);
+        }
+    }
+}
