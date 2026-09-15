daf20395eedac5c598d69c7c1befe4d749a4975e
JGuadalupeD <150277290+JGuadalupeD@users.noreply.github.com>
2026-09-14 22:43:26 -0500

    Modify quicksortrandom to select random pivot
    also changed main to test with a smaller array of size n = 10

--- QuicksortRandom.java
+++ QuicksortRandom.java
@@ -5,7 +5,9 @@ import java.util.Random;
 public class QuicksortRandom {
     public void qsort(TestInteger[] arr, int p, int r){
         if(p < r){
-            int q = Partition(arr, p, r);
+            Random gen = new Random();
+            int q = gen.nextInt(p, r+1);
+            
             qsort(arr, p, q-1);
             qsort(arr, q+1, r); 
         }
@@ -31,20 +33,18 @@ public class QuicksortRandom {
 
     public static void main(String[] args){
         Random gen = new Random();
-        TestInteger[] arr = new TestInteger[10000];
-        for (int i = 0; i<10000;i++){
+        int n = 10;
+        TestInteger[] arr = new TestInteger[n];
+        for (int i = 0; i<n;i++){
             TestInteger num = new TestInteger(gen.nextInt(0,1000000));
             arr[i] = num;
         }
 
         Quicksort qs = new Quicksort();
         qs.qsort(arr,0, arr.length-1);
-        for (int i = 0; i<10000;i++){
+        for (int i = 0; i<n;i++){
             System.out.println(arr[i].value);
         }
-        // Guadalupe D. Tucker J.
-        long timeEnd = System.currentTimeMillis();
-
     }
 
 }
